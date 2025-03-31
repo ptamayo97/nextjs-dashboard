7d5604691f63f04d7f5defefe0335dcca8ffda09
ptamayo97 <ptamayodev@gmail.com>
2025-03-31 15:52:39 -0700

    added middleware

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+
+// Here you're initializing NextAuth.js with the authConfig object and exporting the auth property.
+export default NextAuth(authConfig).auth;
+
+export const config = {
+  // You're also using the matcher option from Middleware to specify that it should run on specific paths.
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
+
+// The advantage of employing Middleware for this task is that
+// the protected routes will not even start rendering until the Middleware verifies the authentication,
+// enhancing both the security and performance of your application.
